5ba6986f30fe9057160ab6eed8dd61382f8d2de5
Eyal Posener <posener@gmail.com>
2020-01-10 23:36:37 +0200

    Fix doc

--- sharedsecret.go
+++ sharedsecret.go
@@ -1,11 +1,10 @@
-/* sharedsecret is implementation of Shamir's Secret Sharing algorithm.
-
-Shamir's Secret Sharing is an algorithm in cryptography created by Adi Shamir. It is a form of
-secret sharing, where a secret is divided into parts, giving each participant its own unique part.
-To reconstruct the original secret, a minimum number of parts is required. In the threshold scheme
-this number is less than the total number of parts. Otherwise all participants are needed to
-reconstruct the original secret. See https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing.
-*/
+// Package sharedsecret is implementation of Shamir's Secret Sharing algorithm.
+//
+// Shamir's Secret Sharing is an algorithm in cryptography created by Adi Shamir. It is a form of
+// secret sharing, where a secret is divided into parts, giving each participant its own unique
+// part. To reconstruct the original secret, a minimum number of parts is required. In the threshold
+// scheme this number is less than the total number of parts. Otherwise all participants are needed
+// to reconstruct the original secret. See https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing.
 package sharedsecret
 
 import (
